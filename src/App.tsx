b40026ad07196df70e4a4c5b067f414af6b7ad61
anderson gil <anderson.gil@karibu.cl>
2022-04-30 09:24:04 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Testing } from './components/Testing';
+
+function App() {
+  return (
+    <div className="App">
+      <Testing>
+        <h1>asdas</h1>
+      </Testing>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Testing.tsx
@@ -0,0 +1,14 @@
+type Props = {
+    children?: React.ReactNode
+};
+
+export const Testing = ({children}:Props) => {
+  return (
+        
+      <>
+          <div>testing</div>  
+          {children}
+      </>
+      
+  )
+}
